4e629f34ed02589c367500b0b2c1f6222703d4e1
VandersonThiago <vandersontpaulo@gmail.com>
2021-09-30 15:43:39 -0300

    Criado a exibição dos slides, agora falta apenas tornar funcional os controles para poder ter a opção de next e previous

--- assets/js/depositions.js
+++ assets/js/depositions.js
@@ -1,23 +1,22 @@
-/*--------------------------------------------Global-Variables-----------------------------------------------------*/
-let amount = document.querySelectorAll('.boxDepositions  .depositionSingle').length;
+/*------------------------------------------------Settings---------------------------------------------------------*/
+let startIn = 1; //don't use 0 here
+let waitingTime = 6000; //use milliseconds here
 /*-----------------------------------------------------------------------------------------------------------------*/
 
 
 
 
-
+let amount = document.querySelectorAll('.boxDepositions  .depositionSingle').length;
 if(amount > 1){
 
     createControlls(amount);
-    
+    slideStart(startIn);
+
 }else{
-    box.innerHTML = "Tem depoimento nao";
+
+    box.innerHTML = "<h1>Não temos nenhum depoimento disponivel no momento!</h1>";
+
 }
-
-
-
-
-
 
 /*--------------------------------------------Functions-Helpers----------------------------------------------------*/
 function createControlls(amount){
@@ -25,5 +24,39 @@ function createControlls(amount){
         let teste = document.createElement("span");
         document.querySelector('.controls').appendChild(teste);
     }
+}
+
+function slideStart(startIn){
+    /*Start*/
+    let balls = document.querySelectorAll('.controls span');
+    let depositions = document.querySelectorAll('.depositionSingle');
+    startIn = startIn -1;
+
+    balls[startIn].classList.add('controlsSelected');
+    depositions[startIn].style.display = 'flex';
+
+    /*Loop*/
+
+    setInterval(function(){
+        for(let i = 0; i < amount; i++){
+            if(balls[i].classList.contains('controlsSelected')){
+                balls[i].classList.remove('controlsSelected')
+                depositions[i].style.display = 'none';
+            }
+        }
+
+        if(startIn < amount-1){
+            startIn++;
+            balls[startIn].classList.add('controlsSelected');
+            depositions[startIn].style.display = 'flex';
+        }else{
+            startIn = 0;
+            balls[startIn].classList.add('controlsSelected');
+            depositions[startIn].style.display = 'flex';
+        }
+
+    }, waitingTime)
+
+
 }
 /*-----------------------------------------------------------------------------------------------------------------*/
